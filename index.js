bd617d9ba80f5facae343254173d24b42e0afc44
Aleph-Phoebe <phbwangui47@gmail.com>
2024-07-06 20:15:50 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(collection) {
+    const yearW = collection.find(years => years.result === "W")
+   
+    if( yearW) {
+     return yearW.year
+    } else {
+     return undefined
+    }
+   }
+
+   //Do some testing here
+console.log(superbowlWin(record));
+   
